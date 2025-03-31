c5e857d237d8e318c71c7d9cf6d0be00ee9b6551
wthug <gdscutsav@gmail.com>
2025-03-31 20:56:09 +0530

    integrated cookies

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -1,20 +1,17 @@
-const User = require("../models/User")
+const User = require("../models/User");
 
 const mailVerify = async (req, res) => {
+  const { uniqueString } = req.params;
 
-    const {uniqueString} = req.params
+  const user = await User.findOne({ uniqueString });
 
-    const user = await User.findOne({uniqueString})
+  if (user) {
+    user.isValid = true;
+    await user.save();
+    res.send("verification was successful");
+  } else {
+    res.send("User not found");
+  }
+};
 
-    if (user) {
-        user.isValid = true
-        await user.save()
-        res.send("verification was successful")
-    }
-    else{
-        res.send("User not found")
-    }
-
-}
-
-module.exports = mailVerify
+module.exports = mailVerify;
