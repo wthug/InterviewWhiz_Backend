1cb40abb9ddc67c8147c9b08051fb771ae372345
wthug <gdscutsav@gmail.com>
2025-04-01 00:01:11 +0530

    small typo fix

--- db/connect.js
+++ db/connect.js
@@ -1,18 +1,17 @@
 // db.js
-const { config } = require('dotenv');
-const mongoose = require('mongoose');
-
+const { config } = require("dotenv");
+const mongoose = require("mongoose");
 
 async function connect() {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true // Remove useCreateIndex option
+      useUnifiedTopology: true,
     });
-    console.log('Connected to MongoDB Atlas');
+    console.log("Connected to MongoDB Atlas");
   } catch (error) {
-    console.error('Error connecting to MongoDB Atlas:', error);
+    console.error("Error connecting to MongoDB Atlas:", error);
   }
 }
 
-module.exports = {connect};
+module.exports = { connect };
