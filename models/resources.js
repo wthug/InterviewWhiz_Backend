f524ff23af14709e6fd5dd07f744ff265ae62c9e
gondaliya utsav <146337919+wthug@users.noreply.github.com>
2025-04-05 15:36:45 +0530

    resource page completed

--- models/resources.js
+++ models/resources.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
 
 const resourceSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  icon: String,
-  link: String,
+  title: {
+    type:String,
+    required:true,
+  },
+  description: {
+    type:String,
+    required:true,
+  },
+  thumbnail: {
+    type:String,
+    required:true,
+  },
+  link: {
+    type:String,
+    required:true,
+  }
 });
 
 const Resource = mongoose.model("Resource", resourceSchema);
