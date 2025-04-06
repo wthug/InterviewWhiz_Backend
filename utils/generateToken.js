41c0d42c516564a69af6f841a8bd3ee96317d30f
wthug <gdscutsav@gmail.com>
2025-04-07 02:10:59 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -3,13 +3,11 @@ const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
-
+  console.log(token);
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true, //prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", //CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "lax",
+    httpOnly: true,
+    secure: true,
     path: "/",
   });
   // res.cookie("testCookie", "testValue", {
